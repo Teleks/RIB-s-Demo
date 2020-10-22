856d18ab835fd7a57b3ec7822519aa8eb6dc4f3c
Nikita Egoshin <nikita.egoshin@omega-r.com>
2020-10-22 20:21:24 +0300

    Implemented weather details

---
+++ WeatherApp/Application/Extensions/Double+Converter.swift
@@ -0,0 +1,8 @@
+//
+//  Double+Converter.swift
+//  WeatherApp
+//
+//  Created by Nikita Egoshin on 22.10.2020.
+//
+
+import Foundation
---
+++ WeatherApp/Model/Entities/City.swift
@@ -0,0 +1,13 @@
+//
+//  City.swift
+//  WeatherApp
+//
+//  Created by Nikita Egoshin on 22.10.2020.
+//
+
+import Foundation
+
+struct CityEntity {
+    let id: String
+    let cityName: String
+}
